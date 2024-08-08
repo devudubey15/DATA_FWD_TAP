15a4c08e2728811fbce9cce6d1279382b23b1d45
devudubey15 <devdubey15091999@gmail.com>
2024-08-08 18:31:30 +0530

    [Final Commit Of The Day] Tested fn_seq_omd

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	// args := os.Args
-	args := []string{"main", "arg1", "arg2", "arg3", "PIPE01", "arg5", "arg6", "arg7"}
+	args := []string{"main", "arg1", "arg2", "arg3", "P1", "arg5", "arg6", "arg7"}
 	serviceName := args[0]
 	var resultTmp int
 
@@ -34,25 +34,28 @@ func main() {
 	*/
 	//----------------------------------------------
 
+	cfgManager := &config.ConfigManager{}
+
 	// Here we are loading PostgreSQL configuration
-	cfg, err := config.LoadPostgreSQLConfig(serviceName, "C:/Users/devdu/go-workspace/data_fwd_tap/config/EnvConfig.ini")
+	cfg, err := cfgManager.LoadPostgreSQLConfig(serviceName, "C:/Users/devdu/go-workspace/data_fwd_tap/config/EnvConfig.ini")
 	if err != nil {
 		log.Fatalf("[%s] Failed to load PostgreSQL configuration: %v", serviceName, err)
 	}
 
-	if config.GetDatabaseConnection(serviceName, *cfg) != 0 {
+	if cfgManager.GetDatabaseConnection(serviceName, *cfg) != 0 {
 		log.Fatalf("[%s] Failed to connect to database", serviceName)
 	}
 
-	DB := config.GetDB(serviceName)
+	DB := cfgManager.GetDB(serviceName)
 	if DB == nil {
 		log.Fatalf("[%s] Database connection is nil. Failed to get the database instance.", serviceName)
 	}
 
-	/* Tuxlib.go Testing
-	tm := &models.TransactionManager{} // This creates a variable of the structure defined in Tuxlib.go. The field in this structure is an instance of the database. Throughout the transaction, we have to use the same database instance.
+	/*Tuxlib.go Testing
 
-	tranType := tm.FnBeginTran(serviceName) // transaction begins here. Here we are calling the "FnBeginTran()" of "Tuxlib.go".
+	txManager := &models.TransactionManager{DbAccessor: cfgManager} // This creates a variable of the structure defined in Tuxlib.go. The field in this structure is an instance of the database. Throughout the transaction, we have to use the same database instance.
+
+	tranType := txManager.FnBeginTran(serviceName) // transaction begins here. Here we are calling the "FnBeginTran()" of "Tuxlib.go".
 
 	if tranType == -1 {
 		log.Fatalf("[%s] Failed to begin transaction", serviceName)
@@ -62,7 +65,7 @@ func main() {
 		Username: "new_user",
 		Email:    "new_user@example.com",
 	}
-	if err := tm.Tran.Create(&user).Error; err != nil {
+	if err := txManager.Tran.Create(&user).Error; err != nil {
 		log.Fatalf("[%s] Failed to insert new user: %v", serviceName, err)
 	}
 
@@ -72,7 +75,7 @@ func main() {
 		Price:         39.99,
 		StockQuantity: 200,
 	}
-	if err := tm.Tran.Create(&product).Error; err != nil {
+	if err := txManager.Tran.Create(&product).Error; err != nil {
 		log.Fatalf("[%s] Failed to insert new product: %v", serviceName, err)
 	}
 
@@ -82,7 +85,7 @@ func main() {
 		Amount:    39.99,
 		Status:    "Pending",
 	}
-	if err := tm.Tran.Create(&order).Error; err != nil {
+	if err := txManager.Tran.Create(&order).Error; err != nil {
 		log.Fatalf("[%s] Failed to insert new order: %v", serviceName, err)
 	}
 
@@ -92,28 +95,28 @@ func main() {
 		Quantity:  1,
 		Price:     39.99,
 	}
-	if err := tm.Tran.Create(&orderItem).Error; err != nil {
+	if err := txManager.Tran.Create(&orderItem).Error; err != nil {
 		log.Fatalf("[%s] Failed to insert new order item: %v", serviceName, err)
 	}
 
 	// Sample Read Operation
 	var fetchedUser structures.User
-	if err := tm.Tran.First(&fetchedUser, "username = ?", "new_user").Error; err != nil {
+	if err := txManager.Tran.First(&fetchedUser, "username = ?", "new_user").Error; err != nil {
 		log.Fatalf("[%s] Failed to fetch user: %v", serviceName, err)
 	}
 	fmt.Printf("[%s] Fetched User: %+v\n", serviceName, fetchedUser)
 
 	// Sample Update Operation
-	if err := tm.Tran.Model(&fetchedUser).Update("Email", "updated_user@example.com").Error; err != nil {
+	if err := txManager.Tran.Model(&fetchedUser).Update("Email", "updated_user@example.com").Error; err != nil {
 		log.Fatalf("[%s] Failed to update user email: %v", serviceName, err)
 	}
 
 	// Sample Delete Operation
-	if err := tm.Tran.Delete(&fetchedUser).Error; err != nil {
+	if err := txManager.Tran.Delete(&fetchedUser).Error; err != nil {
 		log.Fatalf("[%s] Failed to delete user: %v", serviceName, err)
 	}
 
-	if tm.FnCommitTran(serviceName, tranType) != 0 {
+	if txManager.FnCommitTran(serviceName, tranType) != 0 {
 		log.Fatalf("[%s] Failed to commit transaction", serviceName)
 	}
 
